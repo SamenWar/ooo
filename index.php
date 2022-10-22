68a22e230ec03b4421593c7d158ff98e9d30b318
SamenWar <80061124+SamenWar@users.noreply.github.com>
2022-10-22 17:36:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,5 @@
 
     $data = json_decode(file_get_contents('php://input'), true);
     file_get_contents('file.txt', '$data'.print_r($data, 1)."\n", FILE_APPEND);
-    // https://api.telegram.org/bot.TOKEN./setwebhook?url=*ссылка на бота*;
+    // https://api.telegram.org/bot5751752574:AAEchv-AMlPyOQf6l80LGdbbG9My0LlWZww/setwebhook?url=*ссылка на бота*;
     $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
